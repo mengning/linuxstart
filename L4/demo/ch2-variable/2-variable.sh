da75b7fcd56a97fd063e783c418f6c8e512bf96d
guixue <guixue@outlook.com>
2019-03-10 19:29:08 +0800

    [!] update Linux -shell ppt and demo

--- L4/demo/ch2-variable/2-variable.sh
+++ L4/demo/ch2-variable/2-variable.sh
@@ -2,17 +2,17 @@
 
 # 1. 变量的定义 和使用
 my_name=qiyue
-echo 1. $my_name
+echo "1. $my_name"
 
 # 2. 另一种定义方式
 course="linux start"
 echo 2. ${course}
 
-# 3. 只读变量
+# 3. 只读变量, 不加$
 readonly course
 course="linux kernel"
-echo 3. ${course}
+echo "3. ${course}"
 
-# 4. 删除变量
-unset course
-echo 4. ${course}
+# 4. 删除变量, 不加$
+unset my_name
+echo  "4. ${my_name}"
