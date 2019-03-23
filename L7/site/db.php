3ff7315a7bb6c2e80196c7421db565414dedcbfe
guixue <guixue@outlook.com>
2019-03-23 17:59:15 +0800

    add L7 slice and demo

---
+++ L7/site/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "123456";
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "连接成功";
+?>
